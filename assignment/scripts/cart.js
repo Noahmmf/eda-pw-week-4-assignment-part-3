2eb983824749907dbff52c87af6553176c1fcc83
Noahmmf <noahfuglestad@gmail.com>
2023-12-01 10:34:51 -0600

    started q2

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,9 +2,20 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket=[];
 
+function addItem(item) {
+    basket.push(item)
+    return true;
+}
+console.log(addItem('apple'));
+console.log(basket);
 
-
+function listItems(){
+    for(i = 0; i > basket.length; i++);
+    return listItems;
+}
+console.log(listItems());
 
 
 
